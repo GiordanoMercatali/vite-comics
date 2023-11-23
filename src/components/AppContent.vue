c4754026621af53648a853be5de9cb55a6affa9d
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2023-11-23 17:07:06 +0100

    add: logo and nav to header

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -11,5 +11,7 @@ export default {
 </script>
 
 <style scoped lang="scss">
-    
+    main{
+        background-color: gray;
+    }
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,15 +1,92 @@
 <template>
     <header>
-        <h1>Header</h1>
+        <div class="logo">
+            <img src="../assets/img/dc-logo.png" alt="">
+        </div>
+
+        <nav>
+            <ul>
+                <li v-for="item in navItems" :key="item.title">
+                <a href="">{{ item.title }}</a>
+            </li>
+            </ul>
+        </nav>
     </header>
 </template>
 
 <script>
 export default {
+    data(){
+        return{
+            navItems: [
+                {
+                    title: "Characters",
+                },
+
+                {
+                    title: "Comics",
+                },
+
+                {
+                    title: "Movies",
+                },
+
+                {
+                    title: "TV",
+                },
+
+                {
+                    title: "Games",
+                },
+
+                {
+                    title: "Collectibles",
+                },
+
+                {
+                    title: "Videos",
+                },
+
+                {
+                    title: "Fans",
+                },
+
+                {
+                    title: "News",
+                },
+
+                {
+                    title: "Shop",
+                },
+            ]
+        }
+    }
     
 }
 </script>
 
 <style scoped lang="scss">
+@use "../style/partials/mixins" as *;
+
+header {
+    margin-top: 2rem;
+    @include flex($direction: row, $justify-content: space-around, $align-items: center);
+}
+
+ul {
+    @include flex();
+    list-style: none;
+    gap: 1rem;
+
+    a {
+        text-decoration: none;
+        color: black;
+
+        &:hover {
+        color: blue;
+        }
+    }
+
+}
     
 </style>
