a01ca21eb2c4c5038b978003ddce58f79ac95cca
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2023-11-24 17:10:41 +0100

    add: cards (data only)

---
+++ src/components/ComicCard.vue
@@ -0,0 +1,44 @@
+<script>
+
+export default {
+  props: {
+    propThumb: string,
+    propPrice: string,
+    propSeries: string,
+    propType: string
+  },
+};
+</script>
+
+<template>
+  <div class="card">
+    <img :src="propThumb" alt="" />
+    <h3>{{ propSeries }}</h3>
+    <p>{{ propPrice }}</p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use "../style/partials/variables" as *;
+.card {
+    background-color: white;
+    height: 100%;
+
+    img {
+        width: 100%;
+    }
+
+    h3 {
+        text-align: center;
+        color: $primary;
+    }
+
+    /* &.comic-book {
+        background-color: lightblue;
+    }
+
+    &.graphic-novel  {
+        background-color: lightgreen;
+    } */
+}
+</style>
