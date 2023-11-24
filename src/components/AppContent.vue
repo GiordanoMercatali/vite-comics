c6a61d30874ca0e7004dac8e55c7fb2187e23c89
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2023-11-24 10:18:25 +0100

    add: main content blue line

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -1,17 +1,78 @@
 <template lang="">
     <main>
-        <h2>Main</h2>
+        <h2>Content goes here</h2>
+        <div class="section blue-line">
+            <ul>
+                    <li v-for="elem, indexElem in blueSectionItems" :key="elem.img" class="image-container">
+                        <span>{{elem.img}}</span>
+                        <a href=""><img :src="getImage(indexElem)"></a>
+                    </li>
+                </ul>
+        </div>
     </main>
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return {
+            blueSectionItems: [
+                {
+                    img: 'comics-digital-comics'
+                },
+
+                {
+                    img: 'comics-merchandise'
+                },
+
+                {
+                    img: 'comics-shop-locator'
+                },
+
+                {
+                    img: 'comics-subscriptions'
+                },
+
+                {
+                    img: 'dc-power-visa'
+                },
+            ]
+            
+        }
+    },
+
+    methods: {
+        getImage: function (index) {
+            return `../src/assets/img/buy-${this.blueSectionItems[index].img}.png`;
+          },
+    },
 }
 </script>
 
 <style scoped lang="scss">
+@use "../style/partials/mixins" as *;
+@use "../style/partials/variables" as *;
+
     main{
-        background-color: gray;
+        background-color: $main-section-color;
+        color: white;
+
+        .blue-line{
+            background-color: $primary-color;
+            padding: 5rem 0;
+
+            ul {
+                @include flex();
+                gap: 1rem;
+                li {
+                    @include flex();
+                    gap: 1rem;
+                    img{
+                        height: 50%;
+                        width: 50%;
+                    }
+                }
+            }
+        }
     }
 </style>
